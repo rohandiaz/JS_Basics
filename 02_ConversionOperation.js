b1bd997bb54aef459af8f4619aff9cd4d9ca9b4f
rohandiaz <rohanamylasedas@gmail.com>
2024-07-20 19:00:10 +0530

    made changes to the file

--- 02_ConversionOperation.js
+++ 02_ConversionOperation.js
@@ -1,16 +1,30 @@
-console.log("HI!!! My name is Rohan");
+// console.log("HI!!! My name is Rohan");
 
-let isNumber=78947
+// let isNumber=78947
 
-console.log(isNumber);
-let changedNumber=String(isNumber)
+// console.log(isNumber);
+// let changedNumber=String(isNumber)
 
-console.log(changedNumber);
-console.log(typeof changedNumber);
+// console.log(changedNumber);
+// console.log(typeof changedNumber);
 
-let isNumber_01=1
+// let isNumber_01=1
 
-console.log(isNumber_01);
-let changedNumber_01 = Boolean(isNumber_01)
+// console.log(isNumber_01);
+// let changedNumber_01 = Boolean(isNumber_01)
 
-console.log(changedNumber_01);
+// console.log(changedNumber_01);
+
+
+console.log(2%3);
+console.log(2-3);
+console.log(2+3);
+console.log(2*3);
+
+let value=45
+
+let negValue=-value
+console.log(negValue);
+
+console.log(true);
+console.log(+true);
