b4c4dcd1691d8a92a72ee90329840671ff5b7a8c
rohandiaz <rohanamylasedas@gmail.com>
2024-07-21 22:07:39 +0530

    Strings Nos and Maths

---
+++ 04_Strings.js
@@ -0,0 +1,35 @@
+console.log("Hi!!!Its me again");
+
+const name="Rohan"
+const countRepo= 69
+
+// console.log(name + countRepo +"Its really good uk");
+
+console.log(`Hello my name is ${name} and my repo no is ${countRepo}`);
+
+const gameName=new String('Valorant_pubg_GTA 6')
+// console.log(gameName);
+console.log(gameName[9]);
+
+const newString = gameName.substring(0, 10)
+console.log(newString);
+
+const anotherString= gameName.slice(-7,2)
+console.log(anotherString);
+
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.toLowerCase());
+
+console.log(gameName.indexOf('a'));
+console.log(gameName.charAt(10));
+
+const newStringOne = "   rohan_magma    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://rohan.com/magma%69bhowmick"
+
+console.log(url.replace('%69','_'));
+console.log(url.includes('78'))
+console.log(gameName.split('_'));
+console.log(gameName.length);
---
+++ 05_nums&maths.js
@@ -0,0 +1,46 @@
+console.log("Here we go again!!!");
+
+const score=500
+console.log(score);
+
+const balance = new Number(60026)
+console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const newNumber=4561.65419
+// console.log(newNumber.toPrecision(5));
+
+const rupees = 1000000
+// console.log(rupees.toLocaleString('en-IN'));
+
+// ***********MATHS**********//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 69, 8));
+// console.log(Math.max(4, 3, 6, 80));
+
+
+const rand=Math.random()
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.ceil(Math.random()*10) + 1);
+
+console.log(rand);
+console.log((rand*10)+1);
+console.log(Math.ceil(rand*10) + 1);
+console.log(Math.floor(rand*10) + 1);
+
+const max=100
+const min=80
+
+console.log(Math.ceil(rand*(max-min +1)+min));
+console.log(Math.floor(rand*(max-min +1)+min));
+
+
+
