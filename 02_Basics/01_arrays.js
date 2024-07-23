2744a0bd564eb3b85fe72d5e3ec8d54ebfaa526c
rohandiaz <rohanamylasedas@gmail.com>
2024-07-24 00:22:31 +0530

    Arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,38 @@
+console.log("Ah shit!!!...Here we go again");
+
+let myArr=[1,2,3,4,5,6,7,8]
+// console.log(myArr);
+
+const myFriends=["Shounak","Das","Jeet","Poulav"]
+// console.log(myFriends);
+// console.log(myFriends[1]);
+
+// myFriends.push("Somsu")
+// console.log(myFriends);
+// myFriends.pop()
+// myArr.pop()
+// console.log(myFriends);
+// console.log(myArr);
+
+// myArr.unshift(9)
+// console.log(myArr);
+// myArr.shift()
+
+// console.log(myArr);
+// console.log(myArr.includes(6));
+// console.log(myArr.indexOf(6));
+
+// const newArr=myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+
+const myn2=myArr.splice(1,3)
+console.log("B",myArr);
+console.log(myn2);
+
