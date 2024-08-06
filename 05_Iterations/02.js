61a035b4c338014c426e2defc6ccb0210807afc4
rohandiaz <rohanamylasedas@gmail.com>
2024-08-06 22:13:35 +0530

    Whiel n do while

---
+++ 05_Iterations/02.js
@@ -0,0 +1,31 @@
+console.log("Welcome back every1!!!!");
+console.log("Chalia fir");
+
+let i=1
+while (i<17) {
+    // console.log(`The value of i is ${i}`);
+    i+=5
+    
+}
+
+const myArr=["Shounak","Poulav","Jeet","Somsu"]
+let j=0
+
+while (j<myArr.length) {
+    console.log(`The names are: ${myArr[j]}`);
+    j++
+    
+}
+
+// let score=10
+let k=29
+do {
+    // console.log(`The values are ${k}`);
+    k+=2
+} while (k<20);
+
+let score=10
+do {
+    console.log(`The values are ${score}`);
+    score+=2
+} while (score<=20);
