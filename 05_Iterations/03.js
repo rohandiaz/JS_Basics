73fc8331cab342dce24d0b23170f46428f1b71f4
rohandiaz <rohanamylasedas@gmail.com>
2024-08-06 23:03:58 +0530

    for of

---
+++ 05_Iterations/03.js
@@ -0,0 +1,14 @@
+console.log("No cant take it anymore");
+
+const arr=[1, 2, 3, 4, 5, 6,"Rohan"]
+
+for (const i of arr) {
+    console.log(i);
+    
+}
+
+const greetings = "Hello world everyone!!!"
+for (const greet of greetings) {
+    // if(greet===" ") continue;
+    console.log(`Each char is ${greet}`)
+}
